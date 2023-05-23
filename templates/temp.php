52693a2b7864b49d63e505376a8e9559fa66885e
Wra8th <56033607+Wra8th@users.noreply.github.com>
2023-05-23 10:30:35 +0530

    Initial frontend prototype

--- templates/temp.php
+++ templates/temp.php
@@ -24,7 +24,8 @@ if ($result->num_rows > 0) {
     // Output data of each row
     while($row = $result->fetch_assoc()) {
         // echo "id: " . $row["ID"]. " - Humidity: " . $row["humidity"]. " - temperature: " . $row["temperature"]. " - moisture " . $row["moisture"] . "<br>";
-        // $column1 = $row["humidity"];
+        $column1 = $row["humidity"];
+        $column2 = $row["temperature"];
     }
 } else {
     echo "0 results";
@@ -33,6 +34,9 @@ if ($result->num_rows > 0) {
 // Close connection
 $conn->close();
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,36 +44,15 @@ $conn->close();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- <?php echo "<h1> $column1 </h1>" ?> -->
-    <div class="nav-bar">
-
-        <div><a href="/">Home</a></div>
-        <div><a href="/temp">T&H</a></div>
-        <div><a href="/rain">Rain</a></div>
-        <div><a href="/gas">Gas</a></div>
-
+    <div class="container">
+        <h1>Current Temperature and Humidity</h1>
+        <div>
+        <?php echo "<h1> Humidity: $column1 </h1>" ?>
+        <?php echo "<h1> Temperature: $column2 </h1>" ?>
+        </div>
     </div>
     
-    <h1>WELCOME TO FARM WATCH</h1>
-    <div class="flex-container">
-        <div class="splash-image">
-            <img src = "images/main.png">
-        </div>
-        <div class="splash-button">
-            <button>CLICK TO CONTINUE</button>
-        </div>
-        <div id ="content">
-</div>  
-    </div>
-    
-    
-
-
-
-
-    <script src="script.js"></script>
 </body>
 </html>
